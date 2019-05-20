b8e9fa60c44c1c312568a54187c37e2551b1626b
aeulacio <48534112+aeulacio@users.noreply.github.com>
2019-05-20 14:54:06 -0600

    Initial commit

---
+++ precache-manifest.aa63ff7e730df7b898d7054607aef1f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f5b99ef785deea670afb903c1703834",
+    "url": "./index.html"
+  },
+  {
+    "revision": "86ace76e876220049fff",
+    "url": "./static/css/main.65b2a5d6.chunk.css"
+  },
+  {
+    "revision": "82ab71c68fbeb2e27537",
+    "url": "./static/js/2.fcb59f5d.chunk.js"
+  },
+  {
+    "revision": "86ace76e876220049fff",
+    "url": "./static/js/main.c7d55b49.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
